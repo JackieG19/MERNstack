4aaa67bbebee6e1a4aab6c62df4da296be125422
Jackie <jacgut19@gmail.com>
2018-09-25 14:50:21 +0000

    Validation Handlers

--- devconnecter/server.js
+++ devconnecter/server.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const post = require('./routes/api/post')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-// DB config
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-// connect to MongoDB
+// Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log('MongoDB connected'))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport middleware
+app.use(passport.initialize());
 
-// use routes
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-app.use('/api/post', post)
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
