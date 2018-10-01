e9cb34d4738ea3c8fc8ed9d3f19e59e331230b27
Jackie <JGtheophila19@gmail.com>
2018-09-30 21:57:55 -0400

    Load User model and Input Validation

--- devconnecter/routes/api/users.js
+++ devconnecter/routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
